abeeb4694c90181bdb7bd060cadea86d08d5db39
BlackBreezeCastle <2314491271@qq.com>
2021-04-30 22:53:08 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,22 +1,31 @@
 import Korbit
 from Kmath import *
-ob=Korbit.orbit()
-#print(ob.__instance)
 
 def f_at_orbit(r,orb):
     h=Vector3.Tuple3(orb.h()).unit_vector()
     r=Vector3.Tuple3(r)
     r=r-Vector3.Dot(r,h)*h
     pe=Vector3.Tuple3(orb.pe_vector())
-    print(pe)
-    print(r)
     ret=Vector3.Angle(pe,r)
-    if Vector3.Dot(Vector3.Cross(r,pe),h)>0:
+    if Vector3.Dot(Vector3.Cross(r,pe),h)<0:
         ret=-ret
-    print(math.degrees(ret))
+    return ret;
 
 r0=6371000
 h=400000
-ob.set_r_v((0.0,r0+h,0.0),(7900.0,0.0,0.0),0.0,398600446148608.0)
-print(ob.period())
-f_at_orbit((1,1,0),ob)
+
+ob1=Korbit.orbit()
+ob1.set_r_v((0.0,r0+h,0.0),(7800.0,0.0,0.0),0.0,398600446148608.0)
+
+ob2=Korbit.orbit()
+ob2.set_r_v((0.0,r0+h,0.0),(7850.0,0.0,0.0),1000.0,398600446148608.0)
+
+period1=ob1.period()
+angle=0
+for i in range(100000):
+    tmp=f_at_orbit(ob2.state_at_t(period1*i)[0],ob1)
+    if tmp>0 and angle<0 and tmp>angle:
+        print((i,math.degrees(angle)))
+    angle=tmp
+
+	
